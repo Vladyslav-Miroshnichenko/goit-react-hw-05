049740530deeaf031bc034e706bc8f0df55c7466
Vladyslav-Miroshnichenko <bjlag.mupoh@gmail.com>
2024-10-22 20:22:50 +0200

    MoviesPages

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,30 +1,48 @@
 import React, { useEffect, useState } from "react";
-import { searchMovies } from "../api";
+import { searchMovies, fetchTrendingMovies } from "../api";
 import MovieList from "../components/MovieList";
+import { useSearchParams } from "react-router-dom";
 
 const MoviesPage = () => {
-  const [query, setQuery] = useState("");
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [query, setQuery] = useState(searchParams.get("query") || "");
   const [movies, setMovies] = useState([]);
+  const [trendingMovies, setTrendingMovies] = useState([]);
 
   useEffect(() => {
-    if (query) {
-      searchMovies(query).then(setMovies);
-    }
+    const fetchMovies = async () => {
+      if (query) {
+        const results = await searchMovies(query);
+        setMovies(results);
+      } else {
+        const trending = await fetchTrendingMovies();
+        setTrendingMovies(trending);
+      }
+    };
+
+    fetchMovies();
   }, [query]);
 
   const handleSearch = (e) => {
     e.preventDefault();
-    setQuery(e.target.search.value);
+    const newQuery = e.target.search.value;
+    setQuery(newQuery);
+    setSearchParams({ query: newQuery });
   };
 
   return (
     <div>
       <h1>Search Movies</h1>
       <form onSubmit={handleSearch}>
-        <input type="text" name="search" placeholder="Search for a movie" />
+        <input
+          type="text"
+          name="search"
+          placeholder="Search for a movie"
+          defaultValue={query}
+        />
         <button type="submit">Search</button>
       </form>
-      <MovieList movies={movies} />
+      <MovieList movies={query ? movies : trendingMovies} />{" "}
     </div>
   );
 };
